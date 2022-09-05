3a9d9cc4f860aa04ca8db4d6fc0e87b1cb5a0c6e
danielftosta176 <danielftosta@hotmail.com>
2022-09-04 21:31:44 -0300

    Update Client.js

--- Client.js
+++ Client.js
@@ -18,7 +18,7 @@ function mixin(obj1, obj2) {
 
 function Client(uri) {
 	EventEmitter.call(this);
-	this.uri = "wss:///sprightly-brioche-94e58b.netlify.app:3000";
+	this.uri = "wss://w.mpp176.tk";
 	this.ws = undefined;
 	this.serverTimeOffset = 0;
 	this.user = undefined;
