85bf9737855662858378d15da30be04b19a36f42
danielftosta176 <danielftosta@hotmail.com>
2022-10-14 16:43:10 -0300

    Update Client.js

--- Client.js
+++ Client.js
@@ -18,7 +18,7 @@ function mixin(obj1, obj2) {
 
 function Client(uri) {
 	EventEmitter.call(this);
-	this.uri = "wss://w.mpp176.tk:8443";
+	this.uri = "wss://w.mpp176.tk:8443"; //main
 	this.ws = undefined;
 	this.serverTimeOffset = 0;
 	this.user = undefined;
