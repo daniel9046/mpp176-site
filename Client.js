e713c38520b1242509159376d67650912c165249
danielftosta176 <danielftosta@hotmail.com>
2022-10-14 23:25:56 -0300

    Update Client.js

--- Client.js
+++ Client.js
@@ -18,7 +18,7 @@ function mixin(obj1, obj2) {
 
 function Client(uri) {
 	EventEmitter.call(this);
-	this.uri = "wss://w.mpp176.tk:8443"; //main
+	this.uri = "wss://8080-danielftost-mppserverpr-epgh4yyebfn.ws-us71.gitpod.io"; //main
 	this.ws = undefined;
 	this.serverTimeOffset = 0;
 	this.user = undefined;
